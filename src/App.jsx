a64ac355c5061f8132e1e491062ed1c87e55d6fe
stefanobellomo <stefanobellomo1998@gmail.com>
2026-04-21 10:28:08 +0200

    delete app.css from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import DefaultLayout from './layouts/DefaultLayout'
 import ProductDetails from './pages/ProductDetails'
