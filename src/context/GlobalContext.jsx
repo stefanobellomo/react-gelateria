4e692b74ef4d501ff5859dcdf65925f70d0765dc
stefanobellomo <stefanobellomo1998@gmail.com>
2026-04-21 11:00:57 +0200

    create global context and call api

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,2 +1,29 @@
 // http://localhost:3333/products
+import { createContext, useState, useEffect } from "react"
 
+const api_url = import.meta.env.VITE_API_URL
+
+export const GlobalContext = createContext()
+
+export function GlobalProvider({ children }) {
+
+    const [products, setProducts] = useState([])
+
+    const fetchProducts = async () => {
+        try {
+            const response = await fetch(`${api_url}/products`)
+            const data = await response.json()
+
+        } catch (error) {
+            console.error('chiamata api fallita')
+        }
+    }
+
+
+
+    return (
+        <GlobalProvider.Provider>
+            {children}
+        </GlobalProvider.Provider>
+    )
+}
