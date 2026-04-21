be50570a14c383c3cbf3cbccb4e1ece83d36bb92
stefanobellomo <stefanobellomo1998@gmail.com>
2026-04-21 12:38:44 +0200

    refactor ProductDetails component to display product details in a structured layout

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -9,8 +9,29 @@ export default function ProductDetails() {
         <>
             <h1>Lista prodotti</h1>
             {products.map(g => (
-                <p>{g.name}</p>
+                <div key={g.id} className="d-flex">
+                    <h3>{g.name}</h3>
+                    <div>
+                        <span>{g.brand}</span>
+                    </div>
+                    <div>
+                        <img src={g.image} />
+                    </div>
+                </div>
             ))}
         </>
     )
 }
+
+// {
+//     "id": 4,
+//     "brand": "Logitech",
+//     "color": "Black",
+//     "connectivity": "Wireless",
+//     "description": "High precision wireless gaming mouse",
+//     "image": "https://fakeimg.pl/500x500/?text=Wireless+Gaming+Mouse",
+//     "name": "Wireless Gaming Mouse",
+//     "price": 59.99,
+//     "rating": 4.6,
+//     "wireless": true
+// }
