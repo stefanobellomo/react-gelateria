6ad23a3a9a0470419f0d787f0bfd18fd5675011c
stefanobellomo <stefanobellomo1998@gmail.com>
2026-04-21 10:18:29 +0200

    add layout and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import DefaultLayout from './layouts/DefaultLayout'
 
 function App() {
 
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <>
-
+            <h1>Store digitale</h1>
         </>
     )
 }
